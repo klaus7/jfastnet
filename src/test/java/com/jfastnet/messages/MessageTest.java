e7c6557bead5590a00b5a9864baff2594ceba444
Klaus Pfeiffer <kpfeiffer@gmx.at>
2015-12-04 09:58:52 +0100

    long value size test

--- src/test/java/com/jfastnet/messages/MessageTest.java
+++ src/test/java/com/jfastnet/messages/MessageTest.java
@@ -19,7 +19,10 @@ public class MessageTest extends AbstractTest {
 	boolean registerClass = false;
 
 	public static class Message1 extends Message {}
-
+	public static class Message2 extends Message {
+		long value = 0;
+		public Message2(long value) { this.value = value; }
+	}
 
 	@Test
 	public void sizeTest() {
@@ -27,22 +30,43 @@ public class MessageTest extends AbstractTest {
 
 		config = newClientConfig();
 		byte[] b1 = config.serialiser.serialise(new Message1());
-		log.info("size: {}", b1.length);
+		log.info("size unregistered message class: {}", b1.length);
 
 		registerClass = true;
 		config = newClientConfig();
 
 		byte[] b2 = config.serialiser.serialise(new Message1());
-		log.info("size: {}", b2.length);
+		log.info("size registered message class: {}", b2.length);
 
 		assertThat("Message with registered class has to be smaller", b2.length, is(lessThan(b1.length)));
 		assertThat("Naked message should have at max 4 bytes", b2.length, lessThanOrEqualTo(4));
+	}
+
+	@Test
+	public void sizeTestLongValue() {
+		Config config;
+
+		config = newClientConfig();
+		byte[] b1;
+		b1 = config.serialiser.serialise(new Message2(0));
+		log.info("size unreg. zero long value: {}", b1.length);
+		b1 = config.serialiser.serialise(new Message2(Long.MAX_VALUE));
+		log.info("size unreg. max long value:  {}", b1.length);
+
+		registerClass = true;
+		config = newClientConfig();
+
+		b1 = config.serialiser.serialise(new Message2(0));
+		log.info("size reg. zero long value:   {}", b1.length);
+		b1 = config.serialiser.serialise(new Message2(Long.MAX_VALUE));
+		log.info("size reg. max long value:    {}", b1.length);
 	}
 
 	@Override
 	public void customizeKryo(Kryo kryo) {
 		if (registerClass) {
 			kryo.register(Message1.class);
+			kryo.register(Message2.class);
 		}
 	}
 }
